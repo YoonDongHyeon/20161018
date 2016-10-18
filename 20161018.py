e166792f624606992b367583893161726c9a63cc
YoonDongHyeon <dhdh9595@naver.com>
2016-10-18 18:23:14 +0900

    20161018 수업내용

--- 20161018.py
+++ 20161018.py
@@ -1,10 +1,14 @@
 import random
-prob = [0, 0, 0]
-prob[0] = random.randint(0,9)
-prob[1] = random.randint(0,9)
-while prob[0] == prob[1]:
-    prob[1] = random.randint(0, 9)
+prob_rn = [0, 0, 0]
+prob_rn[0] = random.randint(0, 9)
+prob_rn[1] = random.randint(0, 9)
+while prob_rn[0] == prob_rn[1]:
+    prob_rn[1] = random.randint(0, 9)
 
-prob[2] = random.randint(0,9)
-while (prob[0] == prob[2]) or (prob[1] == prob[2]) :
-    prob[2] = random.randint(0, 9)
+prob_rn[2] = random.randint(0, 9)
+while (prob_rn[0] == prob_rn[2]) or (prob_rn[1] == prob_rn[2]) :
+    prob_rn[2] = random.randint(0, 9)
+
+prob = str(prob_rn[0]) + str(prob_rn[1]) + str(prob_rn[2])
+
+ans = input("Input number:")
